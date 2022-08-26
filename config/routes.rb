88ada476bab4f7911087c853e85a27bbca9f948b
Thea Choem <29684683+theachoem@users.noreply.github.com>
2022-08-26 22:52:31 +0700

    close #3 add admin (#4)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :relax_sound_categories
+      resources :products
+      resources :relax_sound_authors
+      resources :relax_sound_translations
+      resources :relax_sounds
+
+      root to: "relax_sound_categories#index"
+    end
   draw(:api)
+  draw(:admin)
   draw(:frontend)
 end
